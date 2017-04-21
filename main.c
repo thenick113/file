162c8be8efbe55458a8ae61c00f9d6663de0c429
HH H <HH H>
2017-04-21 16:20:30 -0700

    new update

--- main.c
+++ main.c
@@ -1,72 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
+struct queue {
+ 	//int currentItem = 0;
+  //int max = 32;
+  //struct stack_item[32];
+}
 
-struct obj {
-	int num;
-	int wait;
+struct queue_item {
+	int numToPrint;
+	int timeToWait;
 };
+
+// Queue / buffer
+// MAX_SIZE = 32
+// isEmpty
+// isFull
+// getFront?
+// dequeue
+// enqueue
 
 //Prototype
 void *print_message_function(void *ptr);
 
 // Producer function
-void *producer(){
-  int producingItem = 1;
+void *producer(void *){
+  int producingItem;
+  int sleepTime;
+  struct queue_item itemToProduce;
+  
   // Process loop
   while(1){
-    // Waits 3-7s before trying to add to the stack
+    producingItem = 1;
     
-    // Produce item to add to the stack
+    // Waits 3-7s before trying to add to the queue
+    sleepTime = rand(4) + 3;
+    sleep(sleepTime);
     
+    // Produce item to add to the queue
+    // Some number that will be printed
+    // Sleep time for consumer between 2 - 9
+    itemToProduce = ge
+      
     // Stack access loop
     while(producingItem){
-    	// Try to block the stack
+    	// Block until queue mutex is available to lock
+      pthread_mutex_lock(&queue_mutex);
       
-      // Check if stack is full
-      if(stackFull()){
-        // Unblock access to stack
-        
-        // Wait
+      // Check if queue is full
+      if(queueFull()){
+        // Unblock access to queue
+        pthread_mutex_unlock(&queue_mutex);
         
       }else{
         // Add item to stack
         
+        // Unblock access to the stack
+        pthread_mutex_unlock(&stack_mutex);
+        
+        //Finish producing current item
+        producingItem = 0;
+    	}
     }
-    
   }
-		
-  
 }
   
 
 
 // Consumer function
-void Consumer() {
-  //check if thread has anything
-  //Get sleep time value then sleep for wait time
-  
+void *consumer(void *) {
+  // Process loop / queue access loop when empty
+  while(1){
+    // Block until queue mutex is available to lock
+    pthread_mutex_lock(&queue_mutex);
+
+    // Check if queue is empty
+    if(stackEmpty()){
+      // Unblock access to queue
+      pthread_mutex_unlock(&queue_mutex);
+
+    }else{
+      // Remove item from queue
+
+      // Unblock access to the queue
+      pthread_mutex_unlock(&queue_mutex);
+      
+      // Wait the items designated amount of time
+      
+      // Print item number
+
+    }
+  }
 }
-// Waits 2-9s before trying to remove from the stack
+
 
 int main() {
     int iret1, iret2;
+  	pthread consumer_thread, producer_thread;
     long t;
 		
-  	// Initialize stack of 32 elements
+  	// Initialize queue of 32 elements
   
-  	// Create mutex for stack
-  	pthread_mutex thread1, thread2;  
+  
+  	// Create mutex for queue
+  	pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;  
   
   	// Create/start producer thread
+  	pthread_create(&producer_thread),
+		               NULL,
+		               producer,
+		               (void*)t);
   
   	// Create/start consumer thread
+  	pthread_create(&consumer_thread),
+		               NULL,
+		               consumer,
+		               (void*)t);
   
   	// Wait until users exits the program
   	// ctrl-c
-  
-iret1 = pthread_create(&thread1, NULL, print_message_function, (void*)t);
+    while(1){
+    }
+  	/*
+	 	iret1 = pthread_create(&thread1, NULL, print_message_function, (void*)t);
     if(iret1) {
         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
         exit(EXIT_FAILURE);
@@ -77,6 +135,7 @@ iret1 = pthread_create(&thread1, NULL, print_message_function, (void*)t);
         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
         exit(EXIT_FAILURE);
     }   
+    */
 
     return 0;
 }
